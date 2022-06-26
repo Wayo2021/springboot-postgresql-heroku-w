ccff398d3d6927acac25be1c10b03018cc887d26
Mr.Wayo Chaiyaphet <studentpig416@gmail.com>
2022-06-26 08:23:40 +0700

    spring boot postgresql heroku deployment repo

---
+++ src/main/java/com/springbootpostgresqlheroku/Wayotest/WayoTestApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpostgresqlheroku.Wayotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootpostgresqlheroku/Wayotest/WayoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootpostgresqlheroku.Wayotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
